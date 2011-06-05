bcc1f8192705d58eefbcbd657c9e707ce3c9d590
Martin Melchior <martin.melchior@fhnw.ch>
2011-06-05 22:42:27 +0200

    Extend from GraphElement

--- src/groovy/org/pillarone/riskanalytics/graph/core/graph/model/Connection.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/core/graph/model/Connection.groovy
@@ -1,16 +1,16 @@
 package org.pillarone.riskanalytics.graph.core.graph.model
 
 
-class Connection {
+class Connection extends GraphElement {
+
+    Port from
+    Port to
+    String comment
 
     public Connection(Port from, Port to) {
         this.from = from
         this.to = to
     }
-
-    Port from
-    Port to
-    String comment
 
     boolean isReplicatingConnection() {
         return from.class == to.class
