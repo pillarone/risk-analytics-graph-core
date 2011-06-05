88fe020292afad68ba677672e5bfc49f37c93e7a
Martin Melchior <martin.melchior@fhnw.ch>
2011-06-05 22:47:47 +0200

    tests for the convenience methods added

--- test/unit/org/pillarone/riskanalytics/graph/core/graph/model/ModelGraphModelTests.groovy
+++ test/unit/org/pillarone/riskanalytics/graph/core/graph/model/ModelGraphModelTests.groovy
@@ -69,4 +69,74 @@ class ModelGraphModelTests extends GroovyTestCase {
         assertSame node2, ports[0].componentNode
 
     }
+
+    void testGetEmergingConnections() {
+        ModelGraphModel model = new ModelGraphModel()
+        ComponentNode node1 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node1")
+        ComponentNode node2 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node2")
+        ComponentNode node3 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node3")
+
+        Connection conn1 = model.createConnection(node2.getPort("outClaims"),node1.getPort("input3"))
+        Connection conn2 = model.createConnection(node2.getPort("outValue1"),node1.getPort("input1"))
+        Connection conn3 = model.createConnection(node3.getPort("outValue2"), node2.getPort("input2"))
+        Connection conn4 = model.createConnection(node3.getPort("outValue2"), node1.getPort("input2"))
+
+     
+        List<Connection> ec = model.getEmergingConnections(node2)
+        assertEquals 3, ec.size()
+        assertTrue ec.contains(conn1)
+        assertTrue ec.contains(conn2)
+        assertTrue ec.contains(conn3)
+        assertFalse ec.contains(conn4)
+
+        ec = model.getEmergingConnections([node1,node2])
+        assertEquals 4, ec.size()
+        assertTrue ec.contains(conn1)
+        assertTrue ec.contains(conn2)
+        assertTrue ec.contains(conn3)
+        assertTrue ec.contains(conn4)
+    }
+
+    void testGetAttachedNodes() {
+        ModelGraphModel model = new ModelGraphModel()
+        ComponentNode node1 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node1")
+        ComponentNode node2 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node2")
+        ComponentNode node3 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node3")
+        ComponentNode node4 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node4")
+
+        Connection conn1 = model.createConnection(node2.getPort("outClaims"),node1.getPort("input3"))
+        Connection conn2 = model.createConnection(node3.getPort("outValue2"), node2.getPort("input2"))
+
+        List<ComponentNode> nodes = model.getAttachedNodes([conn1, conn2])
+        assertEquals 3, nodes.size()
+        assertTrue nodes.contains(node1)
+        assertTrue nodes.contains(node2)
+        assertTrue nodes.contains(node3)
+        assertFalse nodes.contains(node4)
+    }
+
+    void testGetConnectedNodes() {
+        ModelGraphModel model = new ModelGraphModel()
+        ComponentNode node1 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node1")
+        ComponentNode node2 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node2")
+        ComponentNode node3 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node3")
+        ComponentNode node4 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "node4")
+
+        Connection conn1 = model.createConnection(node2.getPort("outClaims"),node1.getPort("input3"))
+        Connection conn2 = model.createConnection(node3.getPort("outValue2"), node2.getPort("input2"))
+
+        List<ComponentNode> nodes = model.getConnectedNodes([node1])
+        assertEquals 2, nodes.size()
+        assertTrue nodes.contains(node1)
+        assertTrue nodes.contains(node2)
+        assertFalse nodes.contains(node3)
+        assertFalse nodes.contains(node4)
+
+        nodes = model.getConnectedNodes([node2])
+        assertEquals 3, nodes.size()
+        assertTrue nodes.contains(node1)
+        assertTrue nodes.contains(node2)
+        assertTrue nodes.contains(node3)
+        assertFalse nodes.contains(node4)
+    }
 }
