f78c97897f91d28e64be7548f5203346fefd7a87
Martin Melchior <martin.melchior@fhnw.ch>
2011-06-05 22:40:51 +0200

    Extend from GraphElement Add boolean method hasPorts()

--- src/groovy/org/pillarone/riskanalytics/graph/core/graph/model/ComponentNode.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/core/graph/model/ComponentNode.groovy
@@ -4,9 +4,8 @@ import org.pillarone.riskanalytics.graph.core.palette.model.ComponentDefinition
 import org.pillarone.riskanalytics.core.components.ComposedComponent
 
 
-class ComponentNode {
+class ComponentNode extends GraphElement {
 
-    String name
     ComponentDefinition type
     List<InPort> inPorts
     List<OutPort> outPorts
@@ -25,4 +24,9 @@ class ComponentNode {
     Port getPort(String name) {
         return (inPorts + outPorts).find { it.name == name}
     }
+
+    boolean hasPorts() {
+        return inPorts?.size()>0 || outPorts?.size()> 0
+    }
+
 }
