f4890fe647a3b56ccce7b681a5348204b46a67ce
Manuel Wyss <manuel.wyss@fhnw.ch>
2011-06-17 10:28:47 +0200

    -extending layout persistence

--- src/groovy/org/pillarone/riskanalytics/graph/core/layout/GraphLayoutService.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/core/layout/GraphLayoutService.groovy
@@ -1,5 +1,9 @@
 package org.pillarone.riskanalytics.graph.core.layout
 
+import org.pillarone.riskanalytics.graph.core.graph.model.ComponentNode
+import org.pillarone.riskanalytics.graph.core.graph.model.AbstractGraphModel
+import org.pillarone.riskanalytics.graph.core.graph.model.ComposedComponentNode
+
 class GraphLayoutService {
 
     private static GraphLayoutService graphLayoutService;
@@ -14,12 +18,17 @@ class GraphLayoutService {
         return graphLayoutService;
     }
 
-    public void saveLayout(long userId, String graphName, Set<ComponentLayout> components) {
+    public boolean findLayout(long userId, String layoutName, String graphName) {
+        return (GraphLayout.find("from GraphLayout where userId=? and layoutName=? and graphModelName=?", [userId, layoutName, graphName]) != null);
+    }
+
+    public void saveLayout(long userId, String layoutName, String graphName, List<ComponentLayout> components) {
         GraphLayout layout;
-        if ((layout = GraphLayout.findByGraphModelNameAndUserId(graphName, userId)) != null) {
+
+        if ((layout = GraphLayout.find("from GraphLayout where userId=? and layoutName=? and graphModelName=?", [userId, layoutName, graphName])) != null) {
             layout.components.clear();
         } else {
-            layout = new GraphLayout(graphModelName: graphName, userId: userId);
+            layout = new GraphLayout(userId: userId, layoutName: layoutName, graphModelName: graphName);
         }
         for (ComponentLayout component: components) {
             layout.addToComponents(component);
@@ -27,10 +36,46 @@ class GraphLayoutService {
         layout.save();
     }
 
-    public Set<ComponentLayout> loadLayout(long userId, String graphName) {
+    public void deleteLayout(long userId, String graphName) {
         GraphLayout layout;
-        if ((layout = GraphLayout.findByGraphModelNameAndUserId(graphName, userId)) == null)
+        if ((layout = GraphLayout.findByGraphModelNameAndUserId(graphName, userId)) != null) {
+            layout.delete();
+        }
+    }
+
+    public List<String> getLayoutsOfGraph(long userId, String graphName) {
+        List<GraphLayout> layouts;
+        List<String> ret = new ArrayList<String>();
+        if ((layouts = GraphLayout.findAllByGraphModelNameAndUserId(graphName, userId)) != null) {
+            for (GraphLayout layout: layouts) {
+                ret.add(layout.layoutName);
+            }
+        }
+        return ret;
+    }
+
+    public Set<ComponentLayout> loadLayout(long userId, String layoutName, String graphName) {
+        GraphLayout layout;
+        if ((layout = GraphLayout.find("from GraphLayout where userId=? and layoutName=? and graphModelName=?", [userId, layoutName, graphName])) == null)
             return null;
         return layout.components;
+    }
+
+    public Map<ComponentNode, ComponentLayout> resolveGraphModel(AbstractGraphModel m, Set<ComponentLayout> layouts) {
+        Map<ComponentNode, ComponentLayout> resolved = new HashMap<ComponentNode, ComponentLayout>();
+        resolveComponentRec(layouts, m, null, resolved)
+        return resolved;
+    }
+
+    private void resolveComponentRec(Set<ComponentLayout> layouts, AbstractGraphModel m, String path, Map<ComponentNode, ComponentLayout> resolved) {
+        for (ComponentNode n: m.allComponentNodes) {
+            ComponentLayout l;
+            String name = (path == null ? "" : path + ".") + n.name;
+            if ((l = layouts.find {it.name.equals(name)}) != null) {
+                resolved.put(n, l);
+                if (n instanceof ComposedComponentNode)
+                    resolveComponentRec(layouts, n.componentGraph, name, resolved);
+            }
+        }
     }
 }
--- test/integration/org/pillarone/riskanalytics/graph/core/layout/LayoutPersistenceTests.groovy
+++ test/integration/org/pillarone/riskanalytics/graph/core/layout/LayoutPersistenceTests.groovy
@@ -4,27 +4,55 @@ import org.pillarone.riskanalytics.graph.core.graph.model.ModelGraphModel
 import org.pillarone.riskanalytics.graph.core.graph.model.ComponentNode
 import org.pillarone.riskanalytics.graph.core.palette.service.PaletteService
 import org.pillarone.riskanalytics.core.example.component.TestComponent
+import org.pillarone.riskanalytics.core.example.component.TestComposedComponent
+import org.pillarone.riskanalytics.graph.core.graph.model.AbstractGraphModel
+import org.pillarone.riskanalytics.graph.core.graph.model.ComposedComponentNode
 
 class LayoutPersistenceTests extends GroovyTestCase {
 
     void testStoreLoad() {
         ModelGraphModel model = new ModelGraphModel("name", "package")
         ComponentNode node = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "name")
-        ComponentNode node2 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComponent), "name2")
+        ComponentNode node2 = model.createComponentNode(PaletteService.instance.getComponentDefinition(TestComposedComponent), "name2")
 
-        Set<ComponentLayout> componentLayouts = new ArrayList<ComponentLayout>()
+        List<ComponentLayout> componentLayouts = new ArrayList<ComponentLayout>()
         int i = 0
         for (ComponentNode n: model.allComponentNodes) {
             componentLayouts.add(new ComponentLayout(name: n.name, type: n.type.typeClass, x: i, y: i));
             i++
         }
-        GraphLayoutService.getInstance().saveLayout(0, model.packageName + "." + model.name, componentLayouts)
+        GraphLayoutService.getInstance().saveLayout(0, "test", model.packageName + "." + model.name, componentLayouts)
 
-        componentLayouts = GraphLayoutService.getInstance().loadLayout(0, model.packageName + "." + model.name);
-        assertEquals model.allComponentNodes.size(), componentLayouts.size()
-        for (i=0;i<componentLayouts.size();i++) {
-            assertNotNull componentLayouts.find{it.x==i}
-            assertNotNull componentLayouts.find{it.y==i}
+        Set<ComponentLayout> result = GraphLayoutService.getInstance().loadLayout(0, "test", model.packageName + "." + model.name);
+        assertEquals model.allComponentNodes.size(), result.size()
+        for (i = 0; i < result.size(); i++) {
+            assertNotNull result.find {it.x == i}
+            assertNotNull result.find {it.y == i}
+        }
+        GraphLayoutService.getInstance().deleteLayout(0, model.packageName + "." + model.name);
+        assertEquals false, GraphLayoutService.getInstance().findLayout(0, "test", model.packageName + "." + model.name)
+
+        GraphLayoutService.getInstance().saveLayout(0, "test", model.packageName + "." + model.name, createLayouts(model));
+        result = GraphLayoutService.getInstance().loadLayout(0, "test", model.packageName + "." + model.name);
+
+        Map<ComponentNode, ComponentLayout> m = GraphLayoutService.getInstance().resolveGraphModel(model, result);
+
+        assertEquals 4, m.values().size();
+
+    }
+
+    List<ComponentLayout> createLayouts(ModelGraphModel m) {
+        List<ComponentLayout> s = new ArrayList<ComponentLayout>();
+        createLayoutsRec(s, m, null);
+        return s;
+    }
+
+    void createLayoutsRec(List<ComponentLayout> layouts, AbstractGraphModel m, String path) {
+        for (ComponentNode n: m.allComponentNodes) {
+            String name = (path == null ? "" : path + ".") + n.name;
+            layouts.add(new ComponentLayout(name: name, type: n.type.typeClass, x: 1, y: 1));
+            if (n instanceof ComposedComponentNode)
+                createLayoutsRec(layouts, n.componentGraph, name);
         }
     }
 }
