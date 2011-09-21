d1264e15bf0736220ed05fafb78b47bbf27ffbc6
Martin Melchior <martin.melchior@fhnw.ch>
2011-09-21 20:57:02 +0200

    Some refactoring: Moving the inclusion of ports in the component nodes to the ComponentNodes

--- src/groovy/org/pillarone/riskanalytics/graph/core/graph/model/ComponentNode.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/core/graph/model/ComponentNode.groovy
@@ -2,6 +2,9 @@ package org.pillarone.riskanalytics.graph.core.graph.model
 
 import org.pillarone.riskanalytics.graph.core.palette.model.ComponentDefinition
 import org.pillarone.riskanalytics.core.components.ComposedComponent
+import java.lang.reflect.Field
+import java.util.HashMap.Entry
+import org.pillarone.riskanalytics.graph.core.graph.wiringvalidation.WiringValidationUtil
 
 
 class ComponentNode extends GraphElement {
@@ -35,4 +38,27 @@ class ComponentNode extends GraphElement {
         return inPorts?.size() > 0 || outPorts?.size() > 0
     }
 
+    public static ComponentNode createInstance(ComponentDefinition definition, String name) {
+        ComponentNode node = new ComponentNode(type: definition, name: name)
+        addPorts(definition, node)
+        return node
+    }
+
+    protected static void addPorts(ComponentDefinition definition, ComponentNode node) {
+        List<InPort> inPorts = []
+        for (Entry<Field, Class> entry in ComponentDefinition.getPortDefinitions(definition, Port.IN_PORT_PREFIX)) {
+            inPorts << new InPort(name: entry.key.name, packetType: entry.value, componentNode: node,
+                    connectionCardinality: WiringValidationUtil.getConnectionCardinality(entry.key),
+                    packetCardinality: WiringValidationUtil.getPacketCardinality(entry.key));
+        }
+        node.inPorts = Collections.unmodifiableList(inPorts)
+
+        List<OutPort> outPorts = []
+        for (Entry<Field, Class> entry in ComponentDefinition.getPortDefinitions(definition, Port.OUT_PORT_PREFIX)) {
+            outPorts << new OutPort(name: entry.key.name, packetType: entry.value, componentNode: node,
+                    connectionCardinality: WiringValidationUtil.getConnectionCardinality(entry.key),
+                    packetCardinality: WiringValidationUtil.getPacketCardinality(entry.key));
+        }
+        node.outPorts = Collections.unmodifiableList(outPorts)
+    }
 }
