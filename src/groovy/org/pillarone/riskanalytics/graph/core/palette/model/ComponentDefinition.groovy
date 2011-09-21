e3abe726b3883ea20714c2e0cba6b02fd79b1843
Martin Melchior <martin.melchior@fhnw.ch>
2011-09-21 20:58:20 +0200

    Some refactoring: Moving the inclusion of ports in the component nodes to the ComponentNodes. Here, the port definitions can be created just from teh component definition.

--- src/groovy/org/pillarone/riskanalytics/graph/core/palette/model/ComponentDefinition.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/core/palette/model/ComponentDefinition.groovy
@@ -1,5 +1,12 @@
 package org.pillarone.riskanalytics.graph.core.palette.model
 
+import java.lang.reflect.Field
+import java.lang.reflect.Type
+import java.lang.reflect.ParameterizedType
+import org.pillarone.riskanalytics.core.components.Component
+import org.pillarone.riskanalytics.core.packets.Packet
+import org.pillarone.riskanalytics.core.packets.PacketList
+
 class ComponentDefinition {
 
     Class typeClass
@@ -24,14 +31,32 @@ class ComponentDefinition {
     private static class ComponentDefinitionComparator<T extends ComponentDefinition> implements Comparator {
 
         int compare(Object o1, Object o2) {
-            return compare((ComponentDefinition) o1, (ComponentDefinition) o2);
+            return compare((ComponentDefinition) o1, (ComponentDefinition) o2)
         }
 
         int compare(ComponentDefinition o1, ComponentDefinition o2) {
-            o1.getSimpleName().compareToIgnoreCase(o2.getSimpleName());
+            o1.getSimpleName().compareToIgnoreCase(o2.getSimpleName())
         }
 
     }
 
+    public static Map<Field, Class> getPortDefinitions(ComponentDefinition definition, String prefix) {
+        Map<String, Class> result = [:]
+        Class currentClass = definition.typeClass
+        while (currentClass != Component.class) {
+            for (Field field in currentClass.declaredFields) {
+                if (field.name.startsWith(prefix) && PacketList.isAssignableFrom(field.type)) {
+                    Class packetType = Packet
+                    Type genericType = field.genericType
+                    if (genericType instanceof ParameterizedType) {
+                        packetType = genericType.actualTypeArguments[0]
+                    }
+                    result.put(field, packetType)
+                }
+            }
+            currentClass = currentClass.superclass
+        }
+        return result
+    }
 
 }
