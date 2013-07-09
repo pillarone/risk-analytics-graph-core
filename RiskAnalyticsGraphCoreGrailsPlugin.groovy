d6633e17e046a4537f0148b3c4241631cb3336f8
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-07-09 13:15:49 +0200

    release new plugin version 1.7

--- RiskAnalyticsGraphCoreGrailsPlugin.groovy
+++ RiskAnalyticsGraphCoreGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RiskAnalyticsGraphCoreGrailsPlugin {
     // the plugin version
-    def version = "0.9"
+    def version = "1.7"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
