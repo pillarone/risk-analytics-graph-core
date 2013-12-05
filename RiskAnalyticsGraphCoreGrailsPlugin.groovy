9d195bac0f886f66b826043fa88953ab12011b8e
detlef-brendle <detlef.brendle@canoo.com>
2013-12-05 13:30:22 +0100

    make plugin a snapshot plugin

--- RiskAnalyticsGraphCoreGrailsPlugin.groovy
+++ RiskAnalyticsGraphCoreGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RiskAnalyticsGraphCoreGrailsPlugin {
     // the plugin version
-    def version = "1.9-a2"
+    def version = "1.9-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,6 +10,7 @@ grails.project.dependency.resolution = {
 
         mavenCentral()
         mavenRepo "https://repository.intuitive-collaboration.com/nexus/content/repositories/pillarone-public/"
+        mavenRepo "https://repository.intuitive-collaboration.com/nexus/content/repositories/pillarone-public-snapshot/"
         mavenRepo "http://repo.spring.io/milestone/" //needed for spring-security-core 2.0-rc2 plugin
     }
 
@@ -23,10 +24,10 @@ grails.project.dependency.resolution = {
         runtime ":spring-security-core:2.0-RC2"
         runtime ":tomcat:7.0.42"
 
-        test ":code-coverage:1.2.6"
+        test ":code-coverage:1.2.7"
 
         if (appName == "risk-analytics-graph-core") {
-            runtime "org.pillarone:risk-analytics-core:1.9-a3"
+            runtime "org.pillarone:risk-analytics-core:1.9-SNAPSHOT"
         }
     }
     dependencies {
@@ -40,11 +41,16 @@ grails.project.dependency.distribution = {
     String scpUrl = ""
     try {
         Properties properties = new Properties()
+        String version = new GroovyClassLoader().loadClass('RiskAnalyticsGraphCoreGrailsPlugin').newInstance().version
         properties.load(new File("${userHome}/deployInfo.properties").newInputStream())
-
         user = properties.get("user")
         password = properties.get("password")
-        scpUrl = properties.get("url")
+
+        if (version?.endsWith('-SNAPSHOT')){
+            scpUrl = properties.get("urlSnapshot")
+        }else {
+            scpUrl = properties.get("url")
+        }
     } catch (Throwable t) {
     }
     remoteRepository(id: "pillarone", url: scpUrl) {
